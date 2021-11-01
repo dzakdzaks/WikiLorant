c52d120510af05fbe9c14d73d69e33baa8302778
Dzaky <dzakyrahmanto@gmail.com>
2021-11-01 16:00:08 +0700

    update corepackage to 1.0.3

--- Module/Home/Package.swift
+++ Module/Home/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.1"),
+        .package(url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.2"),
         .package(path: "../Agent"),
         .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0")
     ],
