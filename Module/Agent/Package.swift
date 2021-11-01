c04091bf6b335768784b0630938d02c2a7fba199
Dzaky <dzakyrahmanto@gmail.com>
2021-11-01 15:17:40 +0700

    update version core package to 1.0.1

--- Module/Agent/Package.swift
+++ Module/Agent/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.0"),
+        .package(url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.1"),
         .package(name: "Realm", url: "https://github.com/realm/realm-cocoa.git", from: "10.18.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0"))
     ],
