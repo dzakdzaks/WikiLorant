7193982b3be23506995be127c8aaf7281442cb3b
Dzaky <dzakyrahmanto@gmail.com>
2021-11-01 17:12:47 +0700

    change method install of core packge

--- Module/DetailAgent/Package.swift
+++ Module/DetailAgent/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.2"),
+        .package(name: "Core", url: "https://github.com/dzakdzaks/CorePackage.git", from: "1.0.2"),
         .package(path: "../Agent"),
         .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", from: "2.0.0")
     ],
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "DetailAgent",
             dependencies: [
-                .product(name: "Core", package: "CorePackage"),
+                "Core",
                 "Agent",
                 "SDWebImageSwiftUI"
             ]),
